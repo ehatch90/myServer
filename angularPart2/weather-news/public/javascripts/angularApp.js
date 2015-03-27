544992742479701c58462471a675285e5dc70702
ehatch90 <ec2-user@ip-172-31-30-77.us-west-2.compute.internal>
2015-03-27 03:02:38 +0000

    stuff to chris

---
+++ angularPart2/weather-news/public/javascripts/angularApp.js
@@ -0,0 +1,72 @@
+angular.module('weatherNews', ['ui.router'])
+.factory('postFactory', ['$http', function($http){
+  var o = {
+    posts: []
+  };
+  o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts);
+    });
+  };
+  o.create = function(post) {
+    return $http.post('/posts', post).success(function(data){
+      o.posts.push(data);
+    });
+  };
+  return o;
+}])
+.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      })
+	.state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostCtrl'
+      });
+    $urlRouterProvider.otherwise('home');
+}])
+.controller('MainCtrl', [
+  '$scope',
+  'postFactory',
+  function($scope, postFactory){
+    postFactory.getAll();
+    $scope.posts = postFactory.posts;
+    $scope.addPost = function() {
+      	if($scope.title === '') { return; }
+    	postFactory.create({
+      		title: $scope.title,
+    	});
+    	$scope.title = '';
+	//$scope.posts.push({title:$scope.formContent,upvotes:0,comments:[]});
+	//scope.formContent='';
+     };
+     $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
+}
+])
+.controller('PostCtrl', [
+  '$scope',
+  '$stateParams',
+  'postFactory', 
+  function($scope, $stateParams, postFactory){
+    $scope.post = postFactory.posts[$stateParams.id];
+    $scope.addComment = function(){
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
+  $scope.incrementUpvotes = function(comment){
+    comment.upvotes += 1; 
+  };
+}]);
